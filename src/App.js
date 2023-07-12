261a2564dba9b3f815520a4888b1109ad85218b8
Girish Singh <girish.singh@thoughtspot.com>
2023-07-12 15:21:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [cluster, setCluster] = React.useState('')
+  const [token, setToken] = React.useState('')
+
+  const handleNext = () => {
+    console.log('handleNext')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='form-element'>
+          <span>ThoughtSpot Cluster</span>
+          <input value={cluster} onChange={(e) => setCluster(e.target.value)}/>
+        </div>
+        <div className='form-element'>
+          <span>Authorization token</span>
+          <input value={token} onChange={(e) => setToken(e.target.value)}/>
+        </div>
+        <div>
+          <button onClick={handleNext}>Next</button>
+        </div>
       </header>
     </div>
   );
